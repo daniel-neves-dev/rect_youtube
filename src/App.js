02bc9306648982f359b55edd9b2fdf85965b2839
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-08-22 09:55:23 -0300

    feat: layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import {BrowserRouter as Router, Routes, Route, Link, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link, } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
 
+import Container from "./layouts/Container";
+import Navbar from "./layouts/Navbar";
+import Footer from "./layouts/Footer";
+import Projects from "./components/pages/Projects";
+
 function App() {
   return (
     <Router>
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/contact">Contato</Link>
-            <Link to="/company">Empresa</Link>
-            <Link to="/newproject">Novo Projeto</Link>
-        </div>
-        <Routes>
-            <Route extct path="/" element={<Home/>}/>
-            <Route path="/company" element={<Company/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="/newproject" element={<NewProject/>}/>
-        </Routes>
-        <h1>Footer</h1>
+        <Navbar/>
+        <Container customClass = 'min-height'>
+            <Routes>
+                <Route extct path="/" element={<Home/>}/>
+                <Route path="/projects" element={<Projects/>}/>
+                <Route path="/company" element={<Company/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/newproject" element={<NewProject/>}/>
+            </Routes>
+        </Container>
+        <Footer/>
     </Router>
   )
 }
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,7 @@
+function Projects(){
+    return(
+        <div>Projetos</div>
+    )
+}
+
+export default Projects
